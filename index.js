9cd1ed2e0a415f0968e9b8c0f510f9f4f9b2a9aa
Pklinhom <piyapatchara.k@gmail.com>
2026-02-24 16:41:17 +0700

    Update upgrade

--- index.js
+++ index.js
@@ -13,47 +13,68 @@ let pickaxeLevel = document.querySelector(".pickaxe-level");
 let pickaxeIncrease = document.querySelector(".pickaxe-increase");
 let pasrsedPickaxeIncrease = parseFloat(pickaxeIncrease.innerHTML)
 
-
+//Gem per Click
 let gpc = 1;
+
+//Gem per Second, passively get more gem with times
+let gps = 0;
 
 function incrementGem(){
     gem.innerHTML = parsedGem
     gem.innerHTML = Math.round(parsedGem += gpc);
 }
 
+setInterval(() => {
+    parsedGem += gps / 10
+    gem.innerHTML = Math.round(parsedGem)
+}, 100)
+
+//Reset Button
 function resetGem(){
     if (parsedGem >= 0){
         parsedGem -= parsedGem;
+
+        gps = 0;
+        /*
+        clickerCost = 10;
+        clickerIncrease = 0;
+        clickerLevel = 0;
+
+        pickaxeCost = 150;
+        pickaxeIncrease = 0;
+        pickaxeLevel = 0;
+        */
         gem.innerHTML = parsedGem;
     }
 }
 
+// Clicker Fucntion
 function buyClicker(){
     if (parsedGem >= parsedClickerCost){
         gem.innerHTML = Math.round(parsedGem -= parsedClickerCost);
 
-        clickerLevel.innerHTML ++
+        clickerLevel.innerHTML ++;
 
-        pasrsedClickerIncrease = parseFloat((pasrsedClickerIncrease * 1.03).toFixed(2))
-        clickerIncrease.innerHTML = pasrsedClickerIncrease
-        gpc += pasrsedClickerIncrease
+        pasrsedClickerIncrease = parseFloat((pasrsedClickerIncrease * 1.05).toFixed(2));
+        clickerIncrease.innerHTML = pasrsedClickerIncrease;
+        gps += pasrsedClickerIncrease;
 
         parsedClickerCost *= 1.55;
-        clickerCost.innerHTML = Math.round(parsedClickerCost)
+        clickerCost.innerHTML = Math.round(parsedClickerCost);
     }
 }
-
+// Pickaxe Function
 function buyPickaxe(){
     if (parsedGem >= parsedPickaxeCost){
         gem.innerHTML = Math.round(parsedGem -= parsedPickaxeCost);
 
-        pickaxeLevel.innerHTML ++
-
-        pasrsedPickaxeIncrease = parseFloat((pasrsedPickaxeIncrease * 1.35).toFixed(2))
-        pickaxeIncrease.innerHTML = pasrsedPickaxeIncrease
-        gpc += pasrsedPickaxeIncrease
+        pickaxeLevel.innerHTML ++;
+    
+        pasrsedPickaxeIncrease = parseFloat((pasrsedPickaxeIncrease * 1.35).toFixed(2));
+        pickaxeIncrease.innerHTML = pasrsedPickaxeIncrease;
+        gps += pasrsedPickaxeIncrease;
 
         parsedPickaxeCost *= 1.75;
-        pickaxeCost.innerHTML = Math.round(parsedPickaxeCost)
+        pickaxeCost.innerHTML = Math.round(parsedPickaxeCost);
     }
 }
